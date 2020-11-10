c6ce4311d821eb37d1cc20d7c05edc82fe4623c0
LouiaR <louitiani@gmail.com>
2020-11-10 10:26:31 +0000

    Add some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow  } from "enzyme";
+import toJson from "enzyme-to-json";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+import CoyoteLogo from "./assets/CSoftware_logo_Neg.svg";
+import App, { Logo } from "./App";
+
+describe("rendering components", () => {
+  it("renders App component without crashing", () => {
+    shallow(<App />);
+  });
+  it("renders App component logo without crashing", () => {
+    const wrapper = shallow(<App />);
+    const header = <Logo src={CoyoteLogo} alt="logo" />;
+    expect(wrapper.contains(header)).toEqual(true);
+  });
+});
+
+describe("snapshots", () => {
+  it("App snapshot", () => {
+    const tree = shallow(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
 });
